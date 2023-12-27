43152506a0bc75de7fbce15a0d251390d6a28da0
amanraj-29 <amanraj.a@prodapt.com>
2023-12-27 12:47:58 +0530

    multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: "dn7gfuosj",
+    api_key:process.env.api_key,
+    api_secret:process.env.api_secret
+})
+
+const uploadOnCloudinary = async (localFilePath) =>{
+    try {
+      if (!localFilePath) return null 
+        
+      const response = await cloudinary.uploader.upload(localFilePath ,{
+        resource_type:"auto"
+      })
+      console.log("File uploaded successfully",response.url)
+      return response;
+    } catch (error) {
+
+      fs.unlinkSync(localFilePath) //remove the locally saved temp files as the upload operation got failed
+      return null;
+      
+    }
+}
+
+export {uploadOnCloudinary}
