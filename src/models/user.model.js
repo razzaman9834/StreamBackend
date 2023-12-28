69d522f0d0c437143e31ec8146f99a9ec7e66451
amanraj-29 <amanraj.a@prodapt.com>
2023-12-28 11:28:37 +0530

    types

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -33,7 +33,7 @@ const userSchema = new Schema({
     },
     watchHistory:
         [{
-            type: Schema.Typers.ObjectId,
+            type: Schema.Types.ObjectId,
             ref: "Video"
         }],
     password: {
@@ -52,7 +52,7 @@ const userSchema = new Schema({
 
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password , 10)
+    this.password =await bcrypt.hash(this.password , 10)
     next()
 })
 
